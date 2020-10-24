647f619f27862d5e3aae3d7f973eadbef53199d0
dylan <dylan.mulligan2020@gmail.com>
2020-10-24 00:50:55 -0400

    Added Module

---
+++ global_constants.js
@@ -0,0 +1,15 @@
+const FILEPATH = '../Tokens/SharkPogBot.txt';
+const PREFIX = '=';
+const BOT_VERSION = "0.1";
+const BOT_NAME = "SharkPogBot";
+const BOT_ID = "750833421252689930";
+const BOT_ADMIN_ROLE_NAME = "SharkPogBotAdmin"
+const MIN_BET_AMOUNT = 500;
+const PERMISSION_DENIED_MESSAGE = "You do not have permission to use this command!";
+const HELP_MESSAGE = "Invalid Arguments, use " + PREFIX +"help <command> for help.";
+const COOLDOWNS = {
+    rob: 60,
+    bankrob: 600
+}
+
+module.exports = { FILEPATH, PREFIX, BOT_VERSION, BOT_NAME, BOT_ID, BOT_ADMIN_ROLE_NAME, MIN_BET_AMOUNT, PERMISSION_DENIED_MESSAGE, HELP_MESSAGE, COOLDOWNS }
